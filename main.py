b0355ac02552c038c651c2fd505f4f079965b321
oastu <oastudev@gmail.com>
2024-12-13 20:40:17 +0100

    session01

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # Typecasting - Process of converting a variable from one data type to another
 
-name = "John Doe"
+name = 'John Doe'
 age = 40
 gpa = 3.2
 is_student = True
 
 gpa = int(gpa) # convert float to int
 print(gpa)
+
+# input() - Promps the user to enter data. Returns the entered data as a string
+name = input("What's your name: ")
+print(f'Hello {name}')
+
+# Math - Arithmetic
+num += 1 # num = num + 1
+num -= 1 # num = num - 1
+num *= 1 # num = num * 1
+num /= 1 # num = num / 1
---
+++ test.py
@@ -0,0 +1,26 @@
+import math
+
+#1 Calculate the radius of a circumference
+
+radius = float(input('Enter the radius of a circle: '))
+
+circumference = 2 * math.pi * radius
+
+print(f"The Circumference is {round(circumference, 2)}cm")
+
+#2 Calculate the area of a circle
+
+radius = float(input('Enter the radius of a circle: '))
+
+area = math.pi * pow(radius, 2)
+
+print(f"The area is {round(area, 2)}cm^2")
+
+#3 Calculate the hypotenuse of a right triangle
+
+a = float(input("Enter  side A: "))
+b = float(input("Enter  side B: "))
+
+c = math.sqrt(pow(a, 2) + pow(b, 2))
+
+print(f"Side c = {c}")
