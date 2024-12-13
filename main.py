f4ef5e5d7fd856d94714f0c953fdc73b84b3efe1
oastu <oastudev@gmail.com>
2024-12-13 17:32:58 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# Typecasting - Process of converting a variable from one data type to another
+
+name = "John Doe"
+age = 40
+gpa = 3.2
+is_student = True
+
+gpa = int(gpa) # convert float to int
+print(gpa)
